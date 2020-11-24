715826f98e9a342fa87b4b60d4cc3f550cd93f99
ilankumaran11 <ilankumaran.i@gmail.com>
2020-11-24 10:49:06 +0530

    Connectable Flux

--- WebFlux-rest-API-Consumer/src/test/java/ConnectableFlux/HotPublisher.java
+++ WebFlux-rest-API-Consumer/src/test/java/ConnectableFlux/HotPublisher.java
@@ -1,5 +1,6 @@
 package ConnectableFlux;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
@@ -10,12 +11,13 @@ import static java.lang.Thread.sleep;
 
 public class HotPublisher {
 
+    Flux<String> stringFlux=Flux.just("A","B","C","D")
+            .delayElements(Duration.ofSeconds(1));
+
     @Test
     public void HotPublisher() throws InterruptedException {
 
-        Flux<String> stringFlux=Flux.just("A","B","C","D")
-                .delayElements(Duration.ofSeconds(1));
-
+        //Below Publish method convert the Flux to ConnectableFlux
         ConnectableFlux<String>  hotPublisher=stringFlux.publish();
         hotPublisher.connect();
 
@@ -23,6 +25,30 @@ public class HotPublisher {
         sleep(2000);
 
         hotPublisher.subscribe(element-> System.out.println("2nd_Subscriber>>>>>>>"+element));
+        sleep(4000);
+    }
+
+
+    @DisplayName("Hot Publisher Expecting at least 2 Subscriber ")
+    @Test
+    public void HotPublisherExpectingSubscriber() throws InterruptedException {
+
+        //Below Publish method convert the Flux to ConnectableFlux
+        ConnectableFlux<String>  hotPublisher=stringFlux.publish();
+
+        /*Below autoConnect method take an input as
+            number of minimum subscriber to be available to emmit the elements.
+              (i.e) If we don't have at least 2 subscribers then Publisher won't emmit it's element.
+         */
+        Flux<String>   expectingSubscriber=hotPublisher.autoConnect(2);
+
+        expectingSubscriber.subscribe(element-> System.err.println("1st_Subscriber>>>>>>>"+element));
+        sleep(2000);
+
+        expectingSubscriber.subscribe(element-> System.out.println("2nd_Subscriber>>>>>>>"+element));
+        sleep(4000);
+
+        expectingSubscriber.subscribe(element-> System.out.println("3nd_Subscriber>>>>>>>"+element));
         sleep(4000);
     }
 }
